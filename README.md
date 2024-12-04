1c9b524fe09fcfc052f2b9f55b01e5decee90b64
Juergen Klug <juergen@jklug.work>
2024-12-04 13:00:27 +0100

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gitlab-ci-python-flask-web-application
 GitLab CI Pipeline the containerizes and deploys a Python Flask web application. 
+
+<br>
+
+Different Dockerfiles are provided to deploy either the Python code, the compiled Python Bytecode, ore the compiled Python Binary.
+
+Define the Dockerfile to use via the DOCKERFILE variable in the CI pipeline manifest, for example: "Dockerfile_PythonCode", "Dockerfile_Multistage_Bytecode", "Dockerfile_Multistage_Binary"
---
+++ flask-app/app.py
@@ -0,0 +1,15 @@
+# Import the Flask class from the Flask library
+from flask import Flask
+
+ # Create an instance of the Flask application
+app = Flask(__name__)
+
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def main():   # Define a function that will execute when the root URL is accessed
+    return "Hi there from Python Flask application\n"  # Return a simple string response to the client.
+
+
+# Start the Flask app on all available network interfaces (0.0.0.0) and port 8080
+app.run(host='0.0.0.0', port=8080)
